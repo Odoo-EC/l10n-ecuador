e1b1088076af4eb0e6fc4011c132393308b54680
Jorge Luis <jorjoluiso@gmail.com>
2023-05-16 11:19:00 -0500

    [FIX] Add mock and patch in environment verification

--- l10n_ec_account_edi/tests/test_l10n_ec_mail.py
+++ l10n_ec_account_edi/tests/test_l10n_ec_mail.py
@@ -1,4 +1,10 @@
+from unittest.mock import patch
+
 from odoo.tests import tagged
+
+from odoo.addons.l10n_ec_account_edi.models.account_edi_document import (
+    AccountEdiDocument,
+)
 
 from .test_edi_common import TestL10nECEdiCommon
 
@@ -8,7 +14,7 @@ from .test_edi_common import TestL10nECEdiCommon
 @tagged("post_install", "mail")
 # class TestL10nMail(TestL10nECEdiCommon, TestMailCommon):
 class TestL10nMail(TestL10nECEdiCommon):
-    def test_l10n_ec_out_invoice(self):
+    def test_l10n_ec_cron_invoice(self):
         # self._init_mail_gateway()
         partner = self.partner_with_email
         self._setup_edi_company_ec()
@@ -18,14 +24,11 @@ class TestL10nMail(TestL10nECEdiCommon):
         invoice.action_post()
         edi_doc = invoice._get_edi_document(self.edi_format)
         edi_doc._process_documents_web_services(with_commit=False)
-        edi_doc.send_mail_to_partner()
+
         # print("Invoice:", invoice.name)
-        # print("Customer:", invoice.partner_id.vat)
-        # print("Customer:", invoice.partner_id.name)
-        # print("Customer email:", invoice.partner_id.email)
-        # print("Move state:", invoice.state)
+        # print("Customer:", invoice.partner_id.vat, invoice.partner_id.name)
+        # print("Email customer:", invoice.partner_id.email)
         # print("Move is_move_sent:", invoice.is_move_sent)
-        # print("Edi state:", invoice.edi_document_ids.state)
         cron_tasks = self.env["ir.cron"].search(
             [
                 (
@@ -37,7 +40,24 @@ class TestL10nMail(TestL10nECEdiCommon):
             ]
         )
         self.assertTrue(cron_tasks)
-        result = cron_tasks.method_direct_trigger()
+
+        def send_mail_to_partners(instance):
+            all_companies = instance.env["res.company"].search(
+                [
+                    ("partner_id.country_id.code", "=", "EC"),
+                    ("l10n_ec_type_environment", "=", "test"),
+                ]
+            )
+            for company in all_companies:
+                instance.with_company(company).send_mail_to_partner()
+
+        with patch.object(
+            AccountEdiDocument,
+            "send_mail_to_partners",
+            send_mail_to_partners,
+        ):
+            result = cron_tasks.method_direct_trigger()
+        # print("Move is_move_sent after trigger:", invoice.is_move_sent)
         self.assertTrue(result)
         self.assertEqual(invoice.state, "posted")
         self.assertTrue(edi_doc.l10n_ec_xml_access_key)
