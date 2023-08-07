7a590288aa3b10a94fa703cfd4b68a7880f27ac3
Ricardo Vinicio Jara Jara <79996025+rvjjcolina@users.noreply.github.com>
2023-08-07 10:10:08 -0500

    [ADD] Requires retentions

--- l10n_ec_withhold/models/account_fiscal_position.py
+++ l10n_ec_withhold/models/account_fiscal_position.py
@@ -4,6 +4,6 @@ from odoo import fields, models
 class AccountFiscalPosition(models.Model):
     _inherit = "account.fiscal.position"
 
-    l10n_ec_withhold = fields.Boolean(
-        string="Requiere Retenciones ?", help="Selecionar si la posición fiscal requiere retenciones"
-    )
+    l10n_ec_withhold = fields.Boolean(string="Requires retentions ?",
+                                      help="Select if the tax position requires withholding"
+                                      )
