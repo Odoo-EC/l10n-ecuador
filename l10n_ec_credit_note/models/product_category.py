947caedc804c58d695ceeb891ef2e898908436cd
Ricardo Vinicio Jara Jara <49212955+rvjaraj@users.noreply.github.com>
2023-10-24 08:34:06 -0500

    Update l10n_ec_credit_note/models/product_category.py
    Co-authored-by: Carlos Lopez <celm1990@hotmail.com>

--- l10n_ec_credit_note/models/product_category.py
+++ l10n_ec_credit_note/models/product_category.py
@@ -9,7 +9,7 @@ class ProductCategory(models.Model):
         "account.account",
         "C.C. Discount",
         company_dependent=True,
-        track_visibility="onchange",
+        tracking=True,
     )
     l10n_ec_property_account_return_id = fields.Many2one(
         "account.account",
